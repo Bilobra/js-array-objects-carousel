074f0b6e5f22b8fa09acf6adc840017f7aa0ab16
Bilobra <bra.carrozzo@gmail.com>
2022-08-04 17:28:08 +0200

    fix slide + active index 0

--- js/slider.js
+++ js/slider.js
@@ -37,24 +37,22 @@ const slides = [
 const slidesWrapper = document.querySelector('.slides-wrapper')
 // funzione che mi genera una slide
 
-function slide(img, i) {
-	const li = document.createElement('li')
-	li.classList.add('slide')
 
-	// const content = document.createElement(<img src="${img.url}" alt="">
-	// <div class="slide__content">
-	// 	<h3 class="slide__title">${img.title}</h3>
-	// 	<p class="slide__description">${img.description}</p>
-	// </div>`)
 
-	if (slides[i] === 0) {
-		li.classList.add('active')
-	}
+slides.forEach((img) => {
 
-	li.append(content)
-	slidesWrapper.append(li)
+	slidesWrapper.innerHTML += `
+	<li class="slide">
+		<img src="${img.url}" alt="">
+	 	<div class="slide__content">
+	 		<h3 class="slide__title">${img.title}</h3>
+	 		<p class="slide__description">${img.description}</p>
+	 	</div>
+	</li>`
 
-}
+})
 
-slides.forEach(slide)
+const liActive = document.querySelectorAll('.slide');
+let imgIndex = 0;
+liActive[imgIndex].classList.add('active')
 
